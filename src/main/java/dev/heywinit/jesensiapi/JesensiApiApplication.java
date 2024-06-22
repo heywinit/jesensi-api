c8873096c1dc7e26a04a1da775bc5d2ab2719449
vineshgh <rvinesh159@gmail.com>
2024-06-22 16:47:04 +0530

    project setup

---
+++ src/main/java/dev/heywinit/jesensiapi/JesensiApiApplication.java
@@ -0,0 +1,11 @@
+package dev.heywinit.jesensiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JesensiApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JesensiApiApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/heywinit/jesensiapi/JesensiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.heywinit.jesensiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JesensiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
